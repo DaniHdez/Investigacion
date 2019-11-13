9ba3ec0e8fa4d51b44374ffd045fdf5b152cf357
DaniHdez <dannyha07@gmail.com>
2019-11-12 19:29:07 -0600

    Intento LSTM paralela

---
+++ LSTMKeras/LSTMParalela/lstmPruebaParalela.py
@@ -0,0 +1,121 @@
+# LSTM  para predecir a los pasajeros de las aerolíneas internacionales
+import numpy
+import matplotlib.pyplot as plt
+from pandas import read_csv
+import math
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+import time
+from systemml.mllearn import Keras2DML
+from pyspark.context import SparkContext 
+from pyspark.sql.session import SparkSession 
+sc = SparkContext('local') 
+spark = SparkSession(sc)
+# from multiprocessing import Process
+# import threading
+
+# convertimos un array de valores en una matriz de conjuntos de datos
+def create_dataset(dataset, look_back=1):
+  dataX, dataY = [], []
+  for i in range(len(dataset)-look_back-1):
+    a = dataset[i:(i+look_back), 0]
+    dataX.append(a)
+    dataY.append(dataset[i + look_back, 0])
+  return numpy.array(dataX), numpy.array(dataY)
+
+# semilla aleatoria para reproducibilidad
+numpy.random.seed(7)
+
+# cargamos el conjunto de datos
+dataframe = read_csv('datos700.csv', usecols=[1], engine='python',skipfooter=3)
+dataset = dataframe.values
+dataset = dataset.astype('float32')
+
+# normalizamos el conjunto de datos
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+
+# dividimos entre entranmiento y test
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+
+# remodelamos X=t y Y=t+1
+look_back = 1
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+
+# remodelamos la entrada para que sea[muestras, pasos de tiempo, características]
+trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+
+#creamos la LSTM network
+model = Sequential()
+model.add(LSTM(4, input_shape=(1, look_back)))
+model.add(Dense(1))
+model.summary()
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+epochs = 100
+batch_size = 1
+samples = 700
+max_iter = int(epochs*math.ceil(samples/batch_size))
+
+keras.backend.set_image_data_format("channels_first")
+
+sysml_model = Keras2DML(spark, model,input_shape=(1,1) )
+
+print ("########################TRAINING#############################################")
+startTraining = time.monotonic()
+#Aca se entrena la red
+sysml_model.fit(trainX, trainY)
+#model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=0)
+finishTraining = time.monotonic()
+print("Tiempo de entrenamiento:", (finishTraining-startTraining))
+
+
+# hacemos las predicciones
+startPredict = time.monotonic()
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+finishPredict = time.monotonic()
+print("Tiempo de predicción:", (finishPredict-startPredict))
+
+# invertimos las predicciones
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform([testY])
+
+# calculamos el error rms
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+print('Resultado del entrenamiento: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Resultado del test: %.2f RMSE' % (testScore))
+
+# predicciones del entrenamiento de cambio para plotear
+trainPredictPlot = numpy.empty_like(dataset)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+
+# predicciones del test de cambio para plotear
+testPredictPlot = numpy.empty_like(dataset)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(dataset))
+
+# ploteamos linea base y predicciones
+plt.plot(trainPredictPlot,'r', linewidth = 2)
+plt.plot(testPredictPlot,'m', linewidth = 2)
+plt.legend( ('Datos', 'Prediccion datos entramiento', 'Prediccion sobre los datos de test'), loc = 'upper left')
+plt.grid(True)
+plt.title("Datos sobre pasajeros por mes desde 1949 a 1960", fontsize = 15)
+plt.xlabel("Tiempo (nº meses)", fontsize = 10)
+plt.ylabel("nº de pasajeros", fontsize = 10)
+plt.show()
